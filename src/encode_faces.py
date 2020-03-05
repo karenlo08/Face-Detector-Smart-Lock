d80a8459595ca4cccbe5ebd48e0ca956abe4cfba
karenlo08 <49169212+karenlo08@users.noreply.github.com>
2020-03-05 09:25:06 -0800

    Add files via upload

---
+++ src/encode_faces.py
@@ -0,0 +1,49 @@
+
+from imutils import paths
+import face_recognition
+import argparse
+import pickle
+import cv2
+import os
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--dataset", required=True,
+	help="path to input directory of faces + images")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+ap.add_argument("-d", "--detection-method", type=str, default="cnn",
+	help="face detection model to use: either `hog` or `cnn`")
+args = vars(ap.parse_args())
+
+# grab the paths to the input images in our dataset
+print("[INFO] quantifying faces...")
+imagePaths = list(paths.list_images(args["dataset"]))
+
+knownEncodings = []
+knownNames = []
+
+for (i, imagePath) in enumerate(imagePaths):
+
+	print("[INFO] processing image {}/{}".format(i + 1,
+		len(imagePaths)))
+	name = imagePath.split(os.path.sep)[-2]
+
+	image = cv2.imread(imagePath)
+	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+	# detect the (x, y)-coordinates of the bounding boxes
+	# corresponding to each face in the input image
+	boxes = face_recognition.face_locations(rgb,
+		model=args["detection_method"])
+
+	encodings = face_recognition.face_encodings(rgb, boxes)
+
+	for encoding in encodings:
+		knownEncodings.append(encoding)
+		knownNames.append(name)
+
+print("[INFO] serializing encodings...")
+data = {"encodings": knownEncodings, "names": knownNames}
+f = open(args["encodings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
